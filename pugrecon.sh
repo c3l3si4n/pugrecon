0e13d7f19d692b50764c9b46b1477bc43308dac0
Daniel M <insidetf2@gmail.com>
2021-03-18 06:36:56 -0300

    added nuclei

--- pugrecon.sh
+++ pugrecon.sh
@@ -7,6 +7,8 @@ else
     export TARGET=$@
     mkdir -p output/$TARGET
     cd output/$TARGET
-    subfinder -d $TARGET -o domains.txt | httpx -o webservers.txt  | nuclei  -t ~/nuclei-templates/files/ -t ~/nuclei-templates/cves/ -t ~/nuclei-templates/security-misconfiguration/ 
-    cat webservers.txt | aquatone
+    subfinder -d $TARGET -o domains.txt | httpx -o webservers.txt -ports 80,8080,8443,8000 -threads 200
+    aquatone < webservers.txt
+	cat webservers.txt | nuclei -t ~/nuclei-templates/ | tee nuclei.txt
+	
 fi;
